4d5f40af8ce37d1c320aa02af6e7967b2f1f42d7
Michael Giglia <michael.a.giglia@gmail.com>
2023-07-06 13:31:15 -0400

    Updated pins to reflect JC docs

--- dev/pico/include/rcc_pins.h
+++ dev/pico/include/rcc_pins.h
@@ -1,11 +1,11 @@
 //Pin Definitions
 //---Motor Controller L298N---//
-#define RCC_ENA 3
-#define RCC_ENB 4
-#define RCC_IN1 5
-#define RCC_IN2 6
-#define RCC_IN3 7
-#define RCC_IN4 8
+#define RCC_ENA 7
+#define RCC_IN1 8
+#define RCC_IN2 9
+#define RCC_IN3 10
+#define RCC_IN4 11
+#define RCC_ENB 12
 //---I2C BUS---//
 #define RCC_I2C_SDA 14
 #define RCC_I2C_SCL 15
@@ -14,3 +14,6 @@
 //---PICO ON BOARD INTERFACES---//
 #define RCC_POTENTIOMETER 28
 #define RCC_PUSHBUTTON 22
+// Optical Interrupt sensors for Odom
+#define RCC_LEFT_ENCODER_PIN_INT  21
+#define RCC_RIGHT_ENCODER_PIN_INT 13
