1083cbef800a33ca550e54d7cfc34530978182ef
Jeannette Circe <jeannettecirce@pinkPro.domain.com>
2023-07-07 21:55:45 -0400

    modified to test all 3 servos

--- dev/pico/tests/back_emf_test.cpp
+++ dev/pico/tests/back_emf_test.cpp
@@ -15,24 +15,28 @@ int main(void)
     
     // cyw43_arch_enable_sta_mode();
     
-    cout << "It inialized?" << "\n";
+    cout << "It initalized?" << "\n";
 
     cyw43_arch_gpio_put(0, true);
 
     Servo s1;
     Servo s2;
+    Servo s3;
     ServoInit(&s1, 16, false, 50);
     ServoInit(&s2, 17, false, 50);
+    ServoInit(&s3, 18, false, 50);
     ServoOn(&s1);
     ServoOn(&s2);
+    ServoOn(&s3);
     
     adc_select_input(2);
     while(true)
     {   
-        // uint16_t pot_val = adc_read();
-        sleep_ms(100);
+        uint16_t pot_val = adc_read();
+        sleep_ms(10);
         ServoPosition(&s1, 100*pot_val/4097);
         ServoPosition(&s2, 100*pot_val/4097);
+        ServoPosition(&s3, 100*pot_val/4097);
         cyw43_arch_gpio_put(0, !(cyw43_arch_gpio_get(0)));
         
 
