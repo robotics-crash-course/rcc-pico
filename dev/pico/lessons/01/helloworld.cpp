1ba4a19bc03c2f7f680e808ea6f5de11c35cc6be
ajaku <andyjaku13@gmail.com>
2023-07-10 22:55:08 -0400

    Add lessons for Software 1 lecture

---
+++ dev/pico/lessons/01/helloworld.cpp
@@ -0,0 +1,16 @@
+#include "rcc_stdlib.h"
+using namespace std;
+
+int main(void)
+{
+    stdio_init_all();
+
+    if(cyw43_arch_init())
+    {
+        cout << "CYW43 init failed!" << "\n";
+    }
+
+    cyw43_arch_gpio_put(0, 1);
+
+    printf("Hello World\n");    
+}
