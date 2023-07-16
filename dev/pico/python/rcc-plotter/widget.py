2edc8e172f3e07d973eb604da7a75de8cfc0cbfa
Michael Giglia <michael.a.giglia@gmail.com>
2023-07-16 13:35:25 -0400

    Got plotter semi-realtime, ~100Hz

--- dev/pico/python/rcc-plotter/widget.py
+++ dev/pico/python/rcc-plotter/widget.py
@@ -4,62 +4,86 @@ from pathlib import Path
 import sys
 
 from PySide2.QtWidgets import QApplication, QWidget, QCheckBox
-from PySide2.QtCore import QFile
+from PySide2 import QtCore
+from PySide2.QtCore import QFile, Signal, Slot, QTimerEvent, QTimer
 from PySide2.QtUiTools import QUiLoader
-from qwt import QwtPlotCurve, QwtPlot
+# from qwt import QwtPlotCurve, QwtPlot
+from pyqtgraph import PlotWidget
 import numpy as np
 from comms.controller import WirelessInterface, WirelessController, packet_receive_demux
 from comms.messages import Sensor_Data
 from threading import Thread
 from time import sleep
+import traceback
+
 
 class Widget(QWidget):
+    msg_sig = Signal(Sensor_Data, QWidget)
+    plotter: PlotWidget
+    timer: QTimer
+    data_line: PlotWidget
+    i=0
+
     def __init__(self):
         super(Widget, self).__init__()
         self.xdata=[0]
         self.ydata=[0]
+        self.interface = WirelessController(WirelessInterface)
         self.load_ui()
-        self.plotter = self.findChildren(QwtPlot, "robotDataPlot")[0]
+        self.plotter = self.findChildren(PlotWidget, "rccPlotter")[0]
+        self.data_line = self.plotter.plot()
+        self.plotter.setYRange(-100, 100, padding=0)
         self.receive_thread = Thread(target=self.start_plotting, daemon=True)
-        self.receive_thread.start()
-        self.plot_thread = Thread(target=self.plot, daemon=True)
-        self.plot_thread.start()
-    
-    def plot(self):
-        while True:
-            sleep(0.01)
-            self.plotter.replot()
-
+        self.msg_sig.connect(self.add_data)
+        self.timer = QTimer()
+        self.timer.setTimerType(QtCore.Qt.PreciseTimer)
+        self.timer.setInterval(10)
+        self.timer.timeout.connect(self.update_plot)
+        self.timer.start()
 
     def load_ui(self):
-        loader = QUiLoader()
-        loader.registerCustomWidget(QwtPlot)
+        self.loader = QUiLoader()
+        self.loader.registerCustomWidget(PlotWidget)
         path = os.fspath(Path(__file__).resolve().parent / "form.ui")
         ui_file = QFile(path)
         ui_file.open(QFile.ReadOnly)
-        loader.load(ui_file, self)
+        self.loader.load(ui_file, self)
         ui_file.close()
 
+    @Slot(Sensor_Data)
+    def add_data(self, data:Sensor_Data):
+        # self.xdata = self.xdata[1:]
+        # self.ydata = self.ydata[1:]
+        self.xdata.append(self.xdata[-1]+1)
+        self.ydata.append(data.angVelZ)
+
+    def update_plot(self):
+        print(f"Plot: {self.i}")
+        self.i = self.i + 1
+        self.data_line.setData(self.xdata[-100:], self.ydata[-100:])
+        # self.plotter.plot(self.xdata[-100:], self.ydata[-100:])
+
     def start_plotting(self):
-        self.curve = QwtPlotCurve("Curve 1")
-        x = np.linspace(0, 1, 101)
-        y = np.linspace(1, 0, 101)
-        self.curve1 = QwtPlotCurve.make(self.xdata, self.ydata, plot=self.plotter)
-        self.interface = WirelessController(WirelessInterface)
         self.interface.packet_receive_process(self.plot_demux, 0.01)
     
     def plot_demux(self, p):
         try:
             if p.id_ == Sensor_Data.id():
                 data = Sensor_Data(p)
-                self.xdata.append(self.xdata[-1]+1)
-                self.ydata.append(data.leftCount) 
-                self.curve1.setData(self.xdata[-100:], self.ydata[-100:])
+                self.msg_sig.emit(data, self)
+                # self.i = self.i + 1
+                # print(f"Recv: {self.i}")
         except:
-            pass
+            print(traceback.format_exc())
+    
+    def start_threads(self):
+        self.receive_thread.start()
+        self.interface.start_inbound()
 
 if __name__ == "__main__":
+    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)  
     app = QApplication([])
     widget = Widget()
+    widget.start_threads()
     widget.show()
     sys.exit(app.exec_())
