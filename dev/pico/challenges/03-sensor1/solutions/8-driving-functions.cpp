d2f2b9476a0d8f9ec93f753f308f9deaa24013ec
jcirce <jett@circe.com>
2023-07-16 20:07:49 -0400

    modified 8 to use pointer to motor struct

--- dev/pico/challenges/03-sensor1/solutions/8-driving-functions.cpp
+++ dev/pico/challenges/03-sensor1/solutions/8-driving-functions.cpp
@@ -1,22 +1,24 @@
 #include "rcc_stdlib.h"
 using namespace std;
 
-Motor motors; 
+//since Motor struct is declared in the scope of main, 
+//need to pass a pointer in our functions
+//functions accept a pointer to Motor, calls it motorPointer
 
-void drive_forwards(void){
+void drive_forwards(Motor* motorPointer){
     int base_power = 50;
     int scale_factor = 1.2;
-    MotorPower(&motors, static_cast<int>(base_power*scale_factor), base_power);
+    MotorPower(motorPointer, static_cast<int>(base_power*scale_factor), base_power);
 }
 
-void drive_backwards(void){
+void drive_backwards(Motor* motorPointer){
     int base_power = 50;
     int scale_factor = 1.2;
-    MotorPower(&motors, static_cast<int>(-1*base_power*scale_factor), -1*base_power);
+    MotorPower(motorPointer, static_cast<int>(-1*base_power*scale_factor), -1*base_power);
 }
 
-void stop(void){
-    MotorPower(&motors, 0, 0);
+void stop(Motor* motorPointer){
+    MotorPower(motorPointer, 0, 0);
 }
 
 int main()
@@ -28,7 +30,7 @@ int main()
 
     rcc_init_pushbutton();
 
-    //see above for motor struct
+    Motor motors; //keeping struct in main's scope
     MotorInit(&motors, RCC_ENA, RCC_ENB, 1000);
     MotorsOn(&motors);
 
@@ -45,15 +47,17 @@ int main()
 
         if(!gpio_get(RCC_PUSHBUTTON))
         {
-            drive_forwards(); 
+            drive_forwards(&motors); 
         }
 
+        //goes forward 200 counts
         if(left_count >= 200 && right_count >= 200){
-            drive_backwards();
+            drive_backwards(&motors);
         }
 
+        //goes backwards 200 more counts
         if(left_count >=400 && right_count >= 400){
-            stop();
+            stop(&motors);
         }
     }
 
