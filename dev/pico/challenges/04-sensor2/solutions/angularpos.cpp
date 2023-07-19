ee97ef4538de7e6f5b7d895f2a4936445529b540
jcirce <jett@circe.com>
2023-07-19 13:08:40 -0400

    working at 500Hz with if statement

--- dev/pico/challenges/04-sensor2/solutions/angularpos.cpp
+++ dev/pico/challenges/04-sensor2/solutions/angularpos.cpp
@@ -14,41 +14,49 @@ int main() {
     MotorInit(&motors, RCC_ENA, RCC_ENB, 1000); //setup 
     MotorsOn(&motors); //enable PWM
 
-    MPU6050 imu; //struct
+    MPU6050 imu; //class setup
     imu.begin(i2c1); //adds to i2c1
     imu.calibrate(); //hold robot still
+    sleep_ms(200);
 
-    int64_t timestep = 2000; //400 Hz in microseconds
-    int64_t current_time = 0;
-    int64_t previous_time = 0;
+    int64_t timestep = 2000; //500 Hz in microseconds
+    // int64_t timestep = 2500; //400 Hz in microseconds
+    // int64_t timestep = 10000; //100Hz
+    // int64_t timestep = 1000; //1kHz
+    int64_t current_time = time_us_64();
+    int64_t previous_time = time_us_64();
     float sum = 0;
     float angvel_z = 0.0;
-    float deltat = 0.002; // in seconds
-    previous_time = time_us_64();
-    current_time = time_us_64();
+    float deltat = 0.002; // 500 Hz in seconds
+    // float deltat = 0.0025; // 400 Hz in seconds
+    // float deltat = 0.01; //100Hz
+    // float deltat = 0.001; //1kHz
     bool stop = false;
     bool moving = false;
 
+    cyw43_arch_gpio_put(0, 0); //turns off led
+    sleep_ms(100);
+    cyw43_arch_gpio_put(0, 1); //turns on led
+
     while(true) 
     {
-        
-        if(!gpio_get(RCC_PUSHBUTTON))
-        {
-            MotorPower(&motors, 50, -50);
+        if(!stop){
+            MotorPower(&motors, -75, 60);
         }
 
-        //this block says wait 
-        while((current_time - previous_time) < timestep){
-            current_time = time_us_64(); //reset current time
+        current_time = time_us_64(); //get current time
+
+        if((current_time - previous_time) >= timestep){ //int math
+            previous_time = current_time; //reset previous time
+            imu.update_pico(); 
+            angvel_z = imu.getAngVelZ(); //deg per sec
+            sum += angvel_z*deltat; //float math
         }
 
-        previous_time = current_time;
-        imu.update_pico(); 
-        angvel_z = imu.getAngVelZ(); //deg per sec
+        cout << sum << '\n';
 
-        sum += angvel_z*deltat;
-
-        if(sum >= (360.0*4.0) || sum <= -(360.0*4.0)){
+        if(sum >= (360.0) || sum <= -(360.0)){
+            cyw43_arch_gpio_put(0, 0); //turns off led
             stop = true;
         }
 
