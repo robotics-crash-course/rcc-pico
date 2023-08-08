eb58c02cae0a290bdd4464168d7e48a37158ab3f
Giglia Admin <michael.a.giglia@gmail.com>
2023-08-08 07:24:10 -0400

    setup pynput to send msgs to pico while also receiving

--- dev/pico/python/keyboard_dev.py
+++ dev/pico/python/keyboard_dev.py
@@ -1,22 +1,32 @@
 from pynput import keyboard
+from comms.messages import Twist, Sensor_Data
+from comms.controller import WirelessController, WirelessInterface, packet_receive_demux
+from comms.packet import Packet
 
-def on_press(key):
+def on_press(key, interface: WirelessController):
     try:
         print('alphanumeric key {0} pressed'.format(
             key.char))
+        if(key.char == 'a'):
+            interface.outbound.put(Twist((1.1, 2.1)).pack())
+
     except AttributeError:
         print('special key {0} pressed'.format(
             key))
 
-def on_release(key):
+def on_release(key, interface):
     print('{0} released'.format(
         key))
     if key == keyboard.Key.esc:
         # Stop listener
         return False
 
-listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+interface = WirelessController(WirelessInterface)
+interface.start_inbound()
+interface.start_outbound()
+
+listener = keyboard.Listener(on_press=lambda key:on_press(key, interface), on_release=lambda key:on_release(key, interface), interface = interface)
 listener.start()
 
 while True:
-    pass
+    interface.packet_receive_process(packet_receive_demux)
