c76aafc448755d0401a72f82f16d45ad00d3a3cc
Michael Giglia <michael.a.giglia@gmail.com>
2023-09-29 12:08:36 -0400

    shrink motor deadband, add error deadband

--- dev/pico/tests/servo_valve.cpp
+++ dev/pico/tests/servo_valve.cpp
@@ -1,15 +1,22 @@
+#define private public
 #include "rcc_stdlib.h"
 using namespace std;
-#define MOV_AVG_SIZE 10
+#define MOV_AVG_SIZE 20
 #define ADC_REF 3.3
 #define SIG_MAX 3.1
-#define SIG_MIN 0.7
+#define SIG_MIN 0.4
 #define SIG_SAFE 3.0
 #define ADC_OUT_MAX 4097
 #define THRESH_HIGH (ADC_OUT_MAX*(SIG_MIN+0.2)/ADC_REF)
 #define THRESH_LOW (ADC_OUT_MAX*(SIG_MIN-0.2)/ADC_REF)
 
 PID_control_config_t con;
+
+typedef struct timing_s{
+    uint32_t dt;
+    uint32_t cur;
+    uint32_t prev;
+} timing_t;
 
 typedef struct avg_s{
     int avg[MOV_AVG_SIZE];
@@ -59,7 +66,7 @@ int main(void){
     sleep_ms(1000);
 
     Motor motors;
-    MotorInit(&motors, RCC_ENA, RCC_ENB, 1000);
+    MotorInit(&motors, RCC_ENA, RCC_ENB, 10000);
     MotorsOn(&motors);    
     
     if(cyw43_arch_init())
@@ -70,17 +77,26 @@ int main(void){
 
     con.kp = 1;
     con.ki = 0.02;
-    con.kd = 0.0;
-    con.ts = 0.01; // 10ms
+    con.kd = 0.0000;
+    con.ts = 0.05; // 10ms
     con.sigma = 0.1;
     con.lowerLimit = -100;
     con.upperLimit = 100;
     con.antiWindupEnabled = true;
     
     PID_control controller(con);
-    controller.setDeadbands(0, 30);
+    controller.setDeadbands(-10, 10);
     Differentiator diff(con.sigma, con.ts);
     avg_t mov_avg;
+    adc_select_input(1);
+    for(int i=0; i<MOV_AVG_SIZE; i++){mov_avg.avg[i]=adc_read();}
+    
+    //Timing interface
+    timing_t time_check;
+    time_check.dt = static_cast<uint32_t>(con.ts*1e6);
+    timing_t led_time;
+    led_time.dt = static_cast<uint32_t>(0.1*1e6);
+
 
     //Init servo valve pot
     adc_init();
@@ -92,40 +108,66 @@ int main(void){
     int plc_val;
     float desired = 2000;
     int plc_raw;
+    int error=0;
 
     hysteresis_t h_state;
 
     while(true) {
-        // Get reading from Potentiometer
-        adc_select_input(0); 
-        servo_pot = adc_read();
-        // Get reading from PLC
-        adc_select_input(1);
-        plc_val = adc_read();
-        plc_raw = plc_val;
-        float plc_float = static_cast<float>(plc_val);
+        uint32_t cur = time_us_32();
+        time_check.cur = cur;
+        led_time.cur = cur;
 
-// Check safe values from adc
-        if(plc_float >= (ADC_OUT_MAX*SIG_MAX/ADC_REF) || plc_float <= (ADC_OUT_MAX*SIG_MIN/ADC_REF))
+        if(time_check.cur-time_check.prev >= time_check.dt)
         {
-            //The voltage input is above 3.1V or < 0.5V, so force plc_val to be full open(3v)
-            plc_val = static_cast<int>(SIG_SAFE*ADC_OUT_MAX/ADC_REF);
-            cout << "\n" << "ERROR, BAD ADC SIG\n";
+            // Get reading from Potentiometer
+            adc_select_input(0); 
+            servo_pot = adc_read();
+            // Get reading from PLC
+            adc_select_input(1);
+            plc_val = adc_read();
+            plc_raw = plc_val;
+            float plc_float = static_cast<float>(plc_val);
+
+    // Check safe values from adc
+            if(plc_float >= (ADC_OUT_MAX*SIG_MAX/ADC_REF) || plc_float <= static_cast<float>(h_state.thresh))
+            {
+                //The voltage input is above 3.1V or < 0.5V, so force plc_val to be full open(3v)
+                plc_val = static_cast<int>(SIG_SAFE*ADC_OUT_MAX/ADC_REF);
+                cout << "\n" << "ERROR, BAD ADC SIG\n";
+                error=-1;
+            }
+            else{ error=0;}
+
+
+    //Calc moving average (after input is agumented if not safe`)
+            int average = sample(&mov_avg, plc_val);
+            update_hysteresis(&h_state, plc_raw);
+            //Do controls
+            float output = controller.pid(average, servo_pot);
+
+            if(abs(controller.error_d1)<5)
+            {
+                MotorPower(&motors, 0, 0);
+
+            }
+            else{
+                //Send controller output
+                MotorPower(&motors, output, 0);
+            }
+            //print for debug
+            // cout << "raw: " << plc_raw << " | h_state: " << h_state.state << '\n';
+            // cout << "raw: " << plc_raw << " | plc_val: " << plc_val << " | avg: " << average << " | servo_pot: " << servo_pot << " | output: " << output << "\n";
+            cout << "$" << average << "," << plc_raw << "," << servo_pot << "," << output << "," << controller.error_d1 << "%\n";
+            //Update prev time var
+            time_check.prev = time_check.cur;
         }
 
-
-//Calc moving average (after input is agumented if not safe`)
-        int average = sample(&mov_avg, plc_val);
-        // update_hysteresis(&h_state, plc_val);
-        //Do controls
-        float output = controller.pid(average, servo_pot);
-
-        //Send controller output
-        MotorPower(&motors, output, 0);
-
-        //print for debug
-        // cout << "raw: " << plc_raw << " | h_state: " << h_state.state << '\n';
-        cout << "raw: " << plc_raw << " | plc_val: " << plc_val << " | avg: " << average << " | servo_pot: " << servo_pot << " | output: " << output << "\n";
+        if(led_time.cur-led_time.prev >= led_time.dt)
+        {
+            if(error<0){ cyw43_arch_gpio_put(0, !cyw43_arch_gpio_get(0));} 
+            else{cyw43_arch_gpio_put(0, 1);}
+            led_time.prev = led_time.cur;
+        }
     }
     
 }
