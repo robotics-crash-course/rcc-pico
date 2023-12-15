cf0e7d7001631761706bb57dedba27ff853a310c
Jeannette <jett@circe.com>
2023-12-15 11:35:51 -0500

    directional odom on level shifted pins

---
+++ dev/pico/using-robot-examples/dirodom.cpp
@@ -0,0 +1,35 @@
+#include "rcc_stdlib.h"
+using namespace std;
+
+int main()
+{
+    stdio_init_all();
+    sleep_ms(1000);
+    cyw43_arch_init();
+    cyw43_arch_gpio_put(0, 1); //turns on led
+
+    Left_Dir_Odom left; //class
+    Right_Dir_Odom right; //class
+    Motor motors;
+
+    MotorInit(&motors, 7, 12, 10000);
+    MotorsOn(&motors);
+
+    while(true)
+    {
+        int left_count = left.getCount(); //gets current count
+        int right_count = right.getCount();
+
+        // //use if want to reset counts
+        // if (left_count>=100){
+        //     left.setZero(); //resets count
+        // }	
+        // if (right_count>=200){
+        //     right.setZero(); //resets count
+        // }
+        MotorPower(&motors, 100, 100);
+
+        cout << left_count << " | " << right_count << "\n";
+        sleep_ms(100);
+    }
+}
